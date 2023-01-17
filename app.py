d24ceb60d646cfb261e0a6967d77519d84aee03a
mbellotti <marianne.bellotti@gmail.com>
2023-01-17 14:40:43 -0500

    route for updating data

--- app.py
+++ app.py
@@ -10,3 +10,21 @@ app = Flask(
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/submit", methods=['POST'])
+def update():
+    if request.method == "POST":
+        key = request.form.get('key')
+        k = int(key)
+        name = request.form.get('name')
+        desc = request.form.get('description')
+    
+        with open("data.json") as f:
+            data = json.loads(f.read())
+            item = data["todo"][k]
+            if name.lower() != "skip":
+                data["done"].append({"pattern":item, "name":name, "description":desc})
+            data["todo"] = data["todo"][0:k] + data["todo"][k+1:]
+    
+        with open("data.json", "w") as f:
+            f.write(json.dumps(data))
